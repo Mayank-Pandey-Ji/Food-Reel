5bbb83d7d9c53a41e7f8130cb4c734bb2b4a6717
Mayank-Pandey-Ji <pandeymayank1099@gmail.com>
2025-11-26 20:51:17 +0530

    change

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,6 +20,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type','Authorization','X-Requested-With','Accept']
 }));
 
+app.options("*", cors());
 
 app.use(cookieParser());
 app.use(express.json());
