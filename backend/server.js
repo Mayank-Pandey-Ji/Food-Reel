176823bf9b08dc250cbe9a751034dfcaf07db38d
Mayank-Pandey-Ji <pandeymayank1099@gmail.com>
2025-11-26 16:10:47 +0530

    changed some code for deployment on vercel

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const connecDB = require('./src/db/db')
 
 connecDB();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+
+// not to use app.listen here as we are using verel for deployment which is serverless
+// app.listen(3000, () => {
+//   console.log('Server is running on port 3000');
+// });
+
+module.exports = app;
