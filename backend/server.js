ad14ab478ab11104aabccc150fb0b9a46eabfe59
Mayank-Pandey-Ji <pandeymayank1099@gmail.com>
2025-11-26 16:41:11 +0530

    changed

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
 // start server 
 require('dotenv').config();
 const app = require('./src/app');
-const connecDB = require('./src/db/db')
+const connectDB = require('./src/db/db')
 
-await connectDB();
+connectDB();
 
 if(process.env.NODE_ENV !== "production")
 {
     const PORT = process.env.PORT || 5000;
-    server.listen(PORT , ()=> console.log("Server is Running on PORT: ", PORT));
+    app.listen(PORT , ()=> console.log("Server is Running on PORT: ", PORT));
 }
 
 // export server for vercel
-export default server;
+module.exports = app;
